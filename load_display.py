483ed00963767c1f5a4e959c16930781976d6533
A1Investment <drew.kessler.21@bishops.com>
2020-09-20 18:14:21 -0700

    added getCardFromUser() function, returns image path of selected card.

--- load_display.py
+++ load_display.py
@@ -3,8 +3,51 @@ import argparse
 
 # testing out jpg input
 ap = argparse.ArgumentParser()
-ap.add_argument("-1","--image",required=True,help="input path to image file")
+ap.add_argument("-1","--image",required=False,help="input path to image file")
 args = vars(ap.parse_args())
+
+def getCardFromUser():
+    """
+    Purpose: To get an image of the desired card from the user.
+    Parameters: None.
+    Return: The image path (str) corresponding to a user-selected card.
+    """
+
+    valueSelected = False # user hasn't selected a value of the card
+    values = list(range(2,11)) + ["J","Q","K","A"] # list of all card values
+    cardValue = ""
+
+    suitSelected = False # user hasn't selected a suit
+    suits = ["Clubs", "Diamonds", "Hearts", "Spades"] # list of 4 suits
+    cardSuit = ""
+
+    while not suitSelected:
+        for (inDex, suit) in enumerate(suits):
+            print(f"{suit}: ({inDex+1})")
+        suitChosen = input("\nChoose your suit by the corresponding righthand index integers: ")
+
+        if suitChosen in ["1","2","3","4"]: # stringified list of possible answer indices
+            suitSelected = True
+            cardSuit = suits[int(suitChosen)-1]
+        else:
+            print("Invalid input. Please choose from 1 to 4, depending on your preferred suit.\n")
+
+    while not valueSelected:
+        for (inDex, value) in enumerate(values):
+            print(f"{value}: ({inDex+1})")
+        valueChosen = input("\nChoose your value by the corresponding righthand index integers: ")
+
+        if valueChosen in ["1","2","3","4","5","6","7","8","9","10","11","12","13"]: # stringified list of possible answer indices
+            valueSelected = True
+            cardValue = values[int(valueChosen)-1]
+        else:
+            print("Invalid input. Please choose from 1 to 13, corresponding to your preferred value.\n")
+
+
+    print(f"\n You've selected the {cardValue} of {cardSuit}!\n")
+    cardImagePath = f"img/cards-[{cardSuit[0]}{cardValue}]-001.jpg" # image Path, the '-001' corresponds to the hardwood background
+    return cardImagePath
+
 
 def display(imagePath, moveOn = False):
     # imagePath str, boolean defaulted to staying at image
@@ -14,4 +57,4 @@ def display(imagePath, moveOn = False):
         cv2.waitKey(0) # move on with any key input
 
 # loading image with cv2
-display(args["image"])
+display(getCardFromUser())
